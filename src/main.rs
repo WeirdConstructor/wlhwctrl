2fd5ed18e54f2de76bc6a574f4eb0957006a8f40
Weird Constructor <weirdconstructor@gmail.com>
2022-02-17 05:11:23 +0100

    implemented timeouted discover devices method

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use std::time::Duration;
 
 
 use futures::executor::block_on;
+use futures::Future;
 use futures::StreamExt;
+use tokio::time::timeout;
 
 use bluer::{
     AdapterEvent,
@@ -47,38 +49,50 @@ impl BluetoothAdapter {
         })
     }
 
-//    pub fn discover_devices(&mut self, timeout: std::time::Duration) -> Vec<> {
-//                let mut disco_events = adapter.discover_devices().await.unwrap();
-//                let mut dev_addr = addrs[0];
-//
-//                while let Some(event) = disco_events.next().await {
-//                    println!("EVENT: {:?}", event);
-//                    match event {
-//                        AdapterEvent::DeviceAdded(addr) => {
-//                            let cur_device = adapter.device(addr).unwrap();
-//                            let name = cur_device.name().await.unwrap();
-//                            println!("Device name: {:?}", name);
-//                            if let Some(name) = name {
-//                                if name == "HC-05" {
-//                                    device = cur_device;
-//                                    dev_addr = addr;
-//                                    break;
-//                                }
-//                            }
-//                        },
-//                        _ => { },
-//                    }
-//                }
-//    }
+    async fn discover_some_devices_impl(&self, devices: &mut Vec<(String, bluer::Address)>) -> Result<(), bluer::Error> {
+        let mut disco_events = self.adapter.discover_devices().await.unwrap();
+
+        while let Some(event) = disco_events.next().await {
+            println!("EVENT: {:?}", event);
+            match event {
+                AdapterEvent::DeviceAdded(addr) => {
+                    let cur_device = self.adapter.device(addr).unwrap();
+                    let name = cur_device.name().await.unwrap();
+                    println!("Device name: {:?}", name);
+                    if let Some(name) = name {
+                        devices.push((name.to_string(), addr));
+                    }
+                },
+                _ => { },
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn discover_some_devices(&mut self, dur: std::time::Duration) -> Result<Vec<(String, bluer::Address)>, bluer::Error> {
+        let mut devices = vec![];
+
+        let rt = self.rt.clone();
+
+        rt.borrow_mut().block_on(async {
+            match timeout(dur, self.discover_some_devices_impl(&mut devices)).await {
+                Ok(r) => r,
+                Err(_) => Ok(()),
+            }
+        })?;
+
+        Ok(devices)
+    }
 }
 
 //#[tokio::main(flavor = "current_thread")]
 fn main() {
-    let rt = tokio::runtime::Builder::new_multi_thread()
+    let rt = Rc::new(RefCell::new(tokio::runtime::Builder::new_multi_thread()
         .worker_threads(1)
         .enable_all()
         .build()
-        .unwrap();
+        .unwrap()));
 
     use wlambda::{GlobalEnv, Env};
     let global_env = GlobalEnv::new_default();
@@ -113,88 +127,94 @@ fn main() {
     bst.fun(
         "list", move |_env: &mut Env, _argc: usize| {
 //            let rt = tokio::runtime::Handle::current();
-            rt.block_on(async {
-                let sess = bluer::Session::new().await.unwrap();
-                // TODO: See: https://github.dev/bluez/bluer/tree/master/bluer
-                // TODO: And: https://docs.rs/bluer/0.13.3/bluer/struct.Session.html#method.new
-                let adapters = sess.adapter_names().await.unwrap();
-                println!("Adapters: {:?}", adapters);
-                let adapter = sess.adapter(adapters.get(0).unwrap()).unwrap();
+            let mut bta = BluetoothAdapter::new(rt.clone()).unwrap();
+            bta.discover_some_devices(Duration::from_secs(1));
+            println!("STOP");
+            bta.discover_some_devices(Duration::from_secs(1));
+            println!("STOP");
+            bta.discover_some_devices(Duration::from_secs(1));
 
-                let addrs = adapter.device_addresses().await.unwrap();
-                println!("Devices: {:#?}", addrs);
-                let mut device = adapter.device(addrs[0]).unwrap();
-                println!("Device name: {:?}", device.name().await.unwrap());
-
-                let mut disco_events = adapter.discover_devices().await.unwrap();
-                let mut dev_addr = addrs[0];
-
-                while let Some(event) = disco_events.next().await {
-                    println!("EVENT: {:?}", event);
-                    match event {
-                        AdapterEvent::DeviceAdded(addr) => {
-                            let cur_device = adapter.device(addr).unwrap();
-                            let name = cur_device.name().await.unwrap();
-                            println!("Device name: {:?}", name);
-                            if let Some(name) = name {
-                                if name == "HC-05" {
-                                    device = cur_device;
-                                    dev_addr = addr;
-                                    break;
-                                }
-                            }
-                        },
-                        _ => { },
-                    }
-                }
-
-                let serial_uuid = ServiceClass::SerialPort.into();
-
-                let agent = Agent::default();
-                let _agent_hndl = sess.register_agent(agent).await.unwrap();
-
-                let profile = Profile {
-                    uuid:                   serial_uuid,
-                    name:                   Some("rfcat client".to_string()),
-                    role:                   Some(Role::Client),
-                    require_authentication: Some(false),
-                    require_authorization:  Some(false),
-                    auto_connect:           Some(true),
-                    ..Default::default()
-                };
-
-                let mut hndl = sess.register_profile(profile).await.unwrap();
-
-                let mut stream = loop {
-                    tokio::select! {
-                        res = async {
-                            let _ = device.connect().await;
-                            device.connect_profile(&serial_uuid).await
-                        } => {
-                            if let Err(err) = res {
-                                println!("Connect profile failed: {:?}", err);
-                            }
-
-                            tokio::time::sleep(Duration::from_secs(3)).await;
-                        },
-                        req = hndl.next() => {
-                            let req = req.unwrap();
-
-                            println!("Connect req (wait for {}): {:?}", dev_addr, req);
-                            if req.device() == dev_addr {
-                                break req.accept().unwrap();
-                                println!("ACCEPT!");
-                            } else {
-                                req.reject(ReqError::Rejected);
-                            }
-                        }
-                    }
-                };
-
-                println!("Connected Stream: {:?}", stream.peer_addr());
-                use tokio::io::AsyncWriteExt;
-                stream.write_all(b"#c22ffff c99ffff ceeffff L0009; %l03!").await.unwrap();
-            });
+//                let sess = bluer::Session::new().await.unwrap();
+//                // TODO: See: https://github.dev/bluez/bluer/tree/master/bluer
+//                // TODO: And: https://docs.rs/bluer/0.13.3/bluer/struct.Session.html#method.new
+//                let adapters = sess.adapter_names().await.unwrap();
+//                println!("Adapters: {:?}", adapters);
+//                let adapter = sess.adapter(adapters.get(0).unwrap()).unwrap();
+//
+//                let addrs = adapter.device_addresses().await.unwrap();
+//                println!("Devices: {:#?}", addrs);
+//                let mut device = adapter.device(addrs[0]).unwrap();
+//                println!("Device name: {:?}", device.name().await.unwrap());
+//
+//                let mut disco_events = adapter.discover_devices().await.unwrap();
+//                let mut dev_addr = addrs[0];
+//
+//                while let Some(event) = disco_events.next().await {
+//                    println!("EVENT: {:?}", event);
+//                    match event {
+//                        AdapterEvent::DeviceAdded(addr) => {
+//                            let cur_device = adapter.device(addr).unwrap();
+//                            let name = cur_device.name().await.unwrap();
+//                            println!("Device name: {:?}", name);
+//                            if let Some(name) = name {
+//                                if name == "HC-05" {
+//                                    device = cur_device;
+//                                    dev_addr = addr;
+//                                    break;
+//                                }
+//                            }
+//                        },
+//                        _ => { },
+//                    }
+//                }
+//
+//                let serial_uuid = ServiceClass::SerialPort.into();
+//
+//                let agent = Agent::default();
+//                let _agent_hndl = sess.register_agent(agent).await.unwrap();
+//
+//                let profile = Profile {
+//                    uuid:                   serial_uuid,
+//                    name:                   Some("rfcat client".to_string()),
+//                    role:                   Some(Role::Client),
+//                    require_authentication: Some(false),
+//                    require_authorization:  Some(false),
+//                    auto_connect:           Some(true),
+//                    ..Default::default()
+//                };
+//
+//                let mut hndl = sess.register_profile(profile).await.unwrap();
+//
+//                let mut stream = loop {
+//                    tokio::select! {
+//                        res = async {
+//                            let _ = device.connect().await;
+//                            device.connect_profile(&serial_uuid).await
+//                        } => {
+//                            if let Err(err) = res {
+//                                println!("Connect profile failed: {:?}", err);
+//                            }
+//
+//                            tokio::time::sleep(Duration::from_secs(3)).await;
+//                        },
+//                        req = hndl.next() => {
+//                            let req = req.unwrap();
+//
+//                            println!("Connect req (wait for {}): {:?}", dev_addr, req);
+//                            if req.device() == dev_addr {
+//                                break req.accept().unwrap();
+//                                println!("ACCEPT!");
+//                            } else {
+//                                req.reject(ReqError::Rejected);
+//                            }
+//                        }
+//                    }
+//                };
+//
+//                println!("Connected Stream: {:?}", stream.peer_addr());
+//                use tokio::io::AsyncWriteExt;
+//                stream.write_all(b"#c22ffff c99ffff ceeffff L0009; %l03!").await.unwrap();
+//            });
 
             Ok(VVal::None)
         }, Some(0), Some(0), false);
